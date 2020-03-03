70372ef0898da987665dc7d8e5770cd28be4dbf3
abdullah ayyaz <abdullahayyaz@tcadmins-MacBook-Air.local>
2020-03-03 11:10:17 +0000

    added text for variable types and data sets

---
+++ 101_python_variables_print_type.py
@@ -0,0 +1,18 @@
+# Variables
+# it is like a box. you give it a name, put stuff inside
+
+book = 'rich dad poor dad'
+
+
+# print function
+# outputs content to the terminal (makes it visible)
+book
+print(book)
+
+# type function
+# allows us to check data types
+data_type_of_book = type(book)
+print(data_type_of_book)
+
+# conventions
+#lower case for variable nameing with underscore
